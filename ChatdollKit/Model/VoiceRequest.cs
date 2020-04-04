547e13ff6c33c789f31d85c5218d48aa3d29f8ea
uezo <uezo@uezo.net>
2020-04-04 20:16:27 +0900

    Add support to change Test-to-Speech speaker temporary #8
    ```Csharp
    // Create TTS options to speak English temporary
    // https://docs.microsoft.com/ja-jp/azure/cognitive-services/speech-service/language-support
    var options = new Dictionary<string, string>()
    {
        { "language", "en-US" },
        { "speakerName", "en-US-JessaRUS" },
    };
    
    // Add voice with options
    animatedVoiceRequest.AddVoiceTTS(translatedText, ttsOptions: options);
    ```

--- ChatdollKit/Model/VoiceRequest.cs
+++ ChatdollKit/Model/VoiceRequest.cs
@@ -23,19 +23,19 @@ namespace ChatdollKit.Model
             }
         }
 
-        public void AddVoice(string name, float preGap = 0.0f, float postGap = 0.0f, string text = null, string url = null, VoiceSource source = VoiceSource.Local)
+        public void AddVoice(string name, float preGap = 0.0f, float postGap = 0.0f, string text = null, string url = null, Dictionary<string, string> ttsOptions = null, VoiceSource source = VoiceSource.Local)
         {
-            Voices.Add(new Voice(name, preGap, postGap, text, url, source));
+            Voices.Add(new Voice(name, preGap, postGap, text, url, ttsOptions, source));
         }
 
         public void AddVoiceWeb(string url, float preGap = 0.0f, float postGap = 0.0f, string name = null, string text = null)
         {
-            Voices.Add(new Voice(name ?? string.Empty, preGap, postGap, text, url, VoiceSource.Web));
+            Voices.Add(new Voice(name ?? string.Empty, preGap, postGap, text, url, null, VoiceSource.Web));
         }
 
-        public void AddVoiceTTS(string text, float preGap = 0.0f, float postGap = 0.0f, string name = null)
+        public void AddVoiceTTS(string text, float preGap = 0.0f, float postGap = 0.0f, string name = null, Dictionary<string, string> ttsOptions = null)
         {
-            Voices.Add(new Voice(name ?? string.Empty, preGap, postGap, text, string.Empty, VoiceSource.TTS));
+            Voices.Add(new Voice(name ?? string.Empty, preGap, postGap, text, string.Empty, ttsOptions, VoiceSource.TTS));
         }
     }
 }
