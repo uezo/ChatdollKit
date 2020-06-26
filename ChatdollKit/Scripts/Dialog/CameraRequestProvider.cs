2361c8249f4017a7838bb73bc2937efa018c9447
uezo <uezo@uezo.net>
2020-06-26 22:16:18 +0900

    Add CameraRequestProvider and QRCodeRequestProvider
    To use these RequestProviders, set RequiredRequestType at your Dialog.
    
    ```Csharp
    // Camera starts at the beginning of the next turn
    context.Topic.RequiredRequestType = RequestType.Camera;
    context.Topic.ContinueTopic = true;
    ```
    
    Note that CameraRequestProvider / QRCodeRequestProvider should be attached to your 3D model.

---
+++ ChatdollKit/Scripts/Dialog/CameraRequestProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using ChatdollKit.IO;
+
+
+namespace ChatdollKit.Dialog
+{
+    public class CameraRequestProvider : MonoBehaviour, IRequestProvider
+    {
+        // This provides photo taken by camera
+        public RequestType RequestType { get; } = RequestType.Camera;
+
+        public string CameraCaption;
+        public int SelfTimerSeconds = 3;
+
+        private ChatdollCamera chatdollCamera;
+
+        private void Start()
+        {
+            chatdollCamera = GameObject.Find("ChatdollCamera").GetComponent<ChatdollCamera>();
+        }
+
+        // Create request using voice recognition
+        public async Task<Request> GetRequestAsync(User user, Context context, CancellationToken token)
+        {
+            var request = new Request(RequestType, user);
+            var payloads = new List<Texture2D>();
+
+            if (chatdollCamera != null)
+            {
+                payloads.Add(await chatdollCamera.CaptureTextureWithTimerAsync(CameraCaption, SelfTimerSeconds));
+            }
+            else
+            {
+                Debug.LogWarning("ChatdollCamera is not found");
+            }
+
+            request.Payloads = payloads;
+            return request;
+        }
+    }
+}
---
+++ ChatdollKit/Scripts/Dialog/QRCodeRequestProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using ChatdollKit.IO;
+
+
+namespace ChatdollKit.Dialog
+{
+    public class QRCodeRequestProvider : MonoBehaviour, IRequestProvider
+    {
+        // This provides decoded QRCode data
+        public RequestType RequestType { get; } = RequestType.QRCode;
+
+        private ChatdollCamera chatdollCamera;
+
+        private void Start()
+        {
+            chatdollCamera = GameObject.Find("ChatdollCamera").GetComponent<ChatdollCamera>();
+        }
+
+        // Create request using voice recognition
+        public async Task<Request> GetRequestAsync(User user, Context context, CancellationToken token)
+        {
+            var request = new Request(RequestType, user);
+            var payloads = new List<string>();
+
+            if (chatdollCamera != null)
+            {
+                payloads.Add(await chatdollCamera.ReadCodeAsync());
+            }
+            else
+            {
+                Debug.LogWarning("ChatdollCamera is not found");
+            }
+
+            request.Payloads = payloads;
+            return request;
+        }
+    }
+}
