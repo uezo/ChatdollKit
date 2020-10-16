e68039338c820ac1c52a63558a61f9a8bf5a7305
uezo <uezo@uezo.net>
2020-10-17 07:12:02 +0900

    Update hello world example

--- Examples/HelloWorld/Scripts/DialogRouter.cs
+++ Examples/HelloWorld/Scripts/DialogRouter.cs
@@ -10,18 +10,10 @@ namespace ChatdollKit.Examples.HelloWorld
         // Extract intent and entities from request and context
         public override async Task<Response> ExtractIntentAsync(Request request, Context context, CancellationToken token)
         {
-            // Static set hello
+            // Always set hello
             request.Intent = "hello";
 
-            // Model actions for this intent
-            var animatedVoiceRequest = new AnimatedVoiceRequest();
-            animatedVoiceRequest.AddVoice("line-girl1-haihaai1", preGap: 1.0f, postGap: 2.0f);
-            animatedVoiceRequest.AddAnimation("Default");
-
-            // Build and return response message
-            var response = new Response(request.Id);
-            response.AnimatedVoiceRequest = animatedVoiceRequest;
-            return response;
+            return new Response(request.Id);
         }
     }
 }
