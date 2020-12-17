5f537e0a44f4b0449cb4f3c0f08ac5091f8cbffb
uezo <uezo@uezo.net>
2020-12-17 22:04:41 +0900

    Enable to set custom message window to VoiceRequestProvider easily
    You can create your own message window by extending `MessageWindowBase` and configure it to VoiceRequestProvider on inspector.
    
    NOTE: `MessageWindowBase` does nothing but just implements methods that `IMessageWindow` requires. This is because Unity 2018 doesn’t allow set variables of interface on inspector.

---
+++ ChatdollKit/Scripts/Dialog/IMessageWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatdollKit.Dialog
+{
+    public interface IMessageWindow
+    {
+        void Show(string prompt = null);
+        void Hide();
+        Task ShowMessageAsync(string message, CancellationToken token);
+        Task SetMessageAsync(string message, CancellationToken token);
+    }
+}
