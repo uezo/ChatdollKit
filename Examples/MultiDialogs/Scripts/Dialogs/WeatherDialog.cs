9faa03cfcf662b982f6964d6a809b764144de598
uezo <uezo@uezo.net>
2020-12-19 12:41:54 +0900

    Add example of app with multiple skills
    If "翻訳" is included in the request, translation skill will be invoked. If "天気", weather skill. The other case echo will be invoked.
    
    - Router: Extract intent from what user says and route to dialog.
    - Translation: Process conversation using external API.
    - Weather: Dummy implementation to test routing.

---
+++ Examples/MultiDialogs/Scripts/Dialogs/WeatherDialog.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using ChatdollKit.Dialog;
+
+namespace ChatdollKit.Examples.MultiDialog
+{
+    public class WeatherDialog : DialogProcessorBase
+    {
+
+#pragma warning disable CS1998
+        public override async Task<Response> ProcessAsync(Request request, Context context, CancellationToken token)
+        {
+            // Get weather
+            var weather = GetWeather(request);
+
+            // Build and return response message
+            var response = new Response(request.Id);
+            response.AddVoiceTTS($"今日の天気は、{weather.Weather}、最高気温は{weather.Temperature}度の見込みです。");
+            response.AddAnimation("Default");
+            return response;
+        }
+#pragma warning restore CS1998
+
+        private WeatherInfo GetWeather(Request request)
+        {
+            // Call weather API here instead
+            return new WeatherInfo();
+        }
+
+        class WeatherInfo
+        {
+            private List<string> weathers = new List<string>() { "晴れ", "晴ときどき曇り", "曇り", "曇りときどき雨", "雨" };
+            public string Weather;
+            public int Temperature;
+
+            public WeatherInfo()
+            {
+                Weather = weathers[Random.Range(0, weathers.Count - 1)];
+                Temperature = Random.Range(0, 40);
+            }
+        }
+    }
+}
