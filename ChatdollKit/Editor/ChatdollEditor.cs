5b3733a5404243d5ef8c0a3ba16c3f4112b8668d
uezo <uezo@uezo.net>
2021-07-15 07:10:50 +0900

    Add editor menu to remove all ChatdollKit components
    Select `Chatdoll > Remove ChatdollKit components` to trigger this feature.

---
+++ ChatdollKit/Editor/ChatdollEditor.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using ChatdollKit;
+using ChatdollKit.Dialog;
+using ChatdollKit.IO;
+using ChatdollKit.Model;
+
+[CustomEditor(typeof(Chatdoll))]
+public class ChatdollEditor : Editor
+{
+    // Remove ChatdollKit components and objects
+    [MenuItem("CONTEXT/Chatdoll/Remove ChatdollKit components")]
+    private static void RemoveComponents(MenuCommand menuCommand)
+    {
+        if (!EditorUtility.DisplayDialog("Confirmation", "Are you sure to remove all ChatdollKit components?", "OK", "Cancel"))
+        {
+            return;
+        }
+
+        var chatdoll = menuCommand.context as Chatdoll;
+        var gameObject = chatdoll.gameObject;
+
+        // Main Application
+        DestroyComponents(gameObject.GetComponents<ChatdollApplication>());
+
+        // RequestProviders and WakeWordListener
+        DestroyComponents(gameObject.GetComponents<IRequestProvider>());
+        DestroyComponents(gameObject.GetComponents<WakeWordListenerBase>());
+
+        // Microphone (Voice recorder depends on this)
+        DestroyComponents(gameObject.GetComponents<ChatdollMicrophone>());
+
+        // Voice loaders
+        DestroyComponents(gameObject.GetComponents<IVoiceLoader>());
+
+        // Prompter
+        DestroyComponents(gameObject.GetComponents<HttpPrompter>());
+
+        // Router
+        DestroyComponents(gameObject.GetComponents<ISkillRouter>());
+
+        // LipSyncHelper
+        DestroyComponents(gameObject.GetComponents<ILipSyncHelper>());
+
+        // Skills
+        DestroyComponents(gameObject.GetComponents<ISkill>());
+
+        // Chatdoll
+        DestroyComponents(gameObject.GetComponents<Chatdoll>());
+
+        // ModelController
+        foreach (var c in gameObject.GetComponents<ModelController>())
+        {
+            // VoiceAudio Control Object
+            DestroyImmediate(c.AudioSource.gameObject);
+            // ModelController itself
+            DestroyImmediate(c);
+        }
+    }
+
+    public static void DestroyComponents(object[] components)
+    {
+        foreach (var c in components)
+        {
+            DestroyImmediate(c as UnityEngine.Object);
+        }
+    }
+}
