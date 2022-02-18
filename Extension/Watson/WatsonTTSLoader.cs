61eb2f550305ceb73718d55af90d5e27e508544c
uezo <uezo@uezo.net>
2022-02-18 20:21:30 +0900

    Add support for Watson Text-to-Speech on WebGL
    Mono / 8000Hz

--- Extension/Watson/WatsonTTSLoader.cs
+++ Extension/Watson/WatsonTTSLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine.Networking;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using ChatdollKit.Model;
-using ChatdollKit.Network;
+using ChatdollKit.IO;
 
 namespace ChatdollKit.Extension.Watson
 {
@@ -36,8 +36,7 @@ namespace ChatdollKit.Extension.Watson
         public string ApiKey = string.Empty;
         public string BaseUrl = string.Empty;
         public string SpeakerName = "ja-JP_EmiV3Voice";
-
-        private ChatdollHttp client = new ChatdollHttp();
+        public AudioType AudioType = AudioType.OGGVORBIS;
 
         public void Configure(string apiKey, string baseUrl, string speakerName, bool overwrite = false)
         {
@@ -46,6 +45,7 @@ namespace ChatdollKit.Extension.Watson
             SpeakerName = string.IsNullOrEmpty(SpeakerName) || overwrite ? speakerName : SpeakerName;
         }
 
+        // See API document: https://cloud.ibm.com/apidocs/text-to-speech#synthesize
         protected override async UniTask<AudioClip> DownloadAudioClipAsync(Voice voice, CancellationToken token)
         {
             if (token.IsCancellationRequested) { return null; };
@@ -56,18 +56,37 @@ namespace ChatdollKit.Extension.Watson
             }
 
             var url = $"{BaseUrl}/v1/synthesize?voice={voice.GetTTSParam("speakerName") as string ?? SpeakerName}";
+
+            var headers = new Dictionary<string, string>()
+            {
+                { "Authorization", client.GetBasicAuthenticationHeaderValue("apikey", ApiKey).ToString() },
+                { "Content-Type", "application/json" },
+                { "Accept", AudioType == AudioType.OGGVORBIS ? "audio/ogg;codecs=vorbis" : "audio/l16;rate=8000" }    // Ogg Vorbis or Wave(mono/8KHz)
+            };
+
+            var data = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new Dictionary<string, string>() { { "text", voice.Text } }));
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+            return await DownloadAudioClipWebGLAsync(url, data, headers, token);
+#else
+            return await DownloadAudioClipNativeAsync(url, data, headers, token);
+#endif
+        }
+
+        protected async UniTask<AudioClip> DownloadAudioClipNativeAsync(string url, byte[] data, Dictionary<string, string> headers, CancellationToken token)
+        {
             using (var www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.OGGVORBIS))
             {
                 www.timeout = Timeout;
                 www.method = "POST";
 
                 // Header
-                www.SetRequestHeader("Authorization", client.GetBasicAuthenticationHeaderValue("apikey", ApiKey).ToString());
-                www.SetRequestHeader("Content-Type", "application/json");
-                www.SetRequestHeader("Accept", "audio/ogg;codecs=vorbis");
+                www.SetRequestHeader("Authorization", headers["Authorization"]);
+                www.SetRequestHeader("Content-Type", headers["Content-Type"]);
+                www.SetRequestHeader("Accept", headers["Accept"]);
 
                 // Body
-                www.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new Dictionary<string, string>(){{"text", voice.Text}})));
+                www.uploadHandler = new UploadHandlerRaw(data);
 
                 // Send request
                 await www.SendWebRequest().ToUniTask();
@@ -82,6 +101,12 @@ namespace ChatdollKit.Extension.Watson
                 }
             }
             return null;
+        }
+
+        protected async UniTask<AudioClip> DownloadAudioClipWebGLAsync(string url, byte[] data, Dictionary<string, string> headers, CancellationToken token)
+        {
+            var resp = await client.PostBytesAsync(url, data, headers, cancellationToken: token);
+            return AudioConverter.PCMToAudioClip(resp.Data, 1, 8000);
         }
     }
 }
