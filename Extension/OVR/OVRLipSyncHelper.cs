5b86e17ed4ef98fa6660af0f2e40a4217538f802
uezo <uezo@uezo.net>
2022-12-19 19:32:01 +0900

    Align internal logic of OVRLipSyncHelper and uLipSyncHelper

--- Extension/OVR/OVRLipSyncHelper.cs
+++ Extension/OVR/OVRLipSyncHelper.cs
@@ -10,17 +10,12 @@ namespace ChatdollKit.Extension.OVR
     {
         public OVRLipSyncContext LipSyncContext;
 
-        public void Reset()
-        {
-            // Configure viseme when attached or reset
-            ConfigureViseme();
-        }
-
         public void ResetViseme()
         {
             LipSyncContext.ResetContext();
         }
 
+#if UNITY_EDITOR
         public void ConfigureViseme()
         {
             // Get GameObjects
@@ -41,26 +36,33 @@ namespace ChatdollKit.Extension.OVR
 
             // Configure OVRLipSyncContextMorphTarget
             morphTarget.skinnedMeshRenderer = modelController.SkinnedMeshRenderer;
-            var shapeKeyIndexes = GetVisemeTargetShapeKeyIndexes(morphTarget.skinnedMeshRenderer.sharedMesh);
-            if (shapeKeyIndexes.Count(v => v == -1) > 0)
-            {
-                Debug.LogWarning("Can't get all shapekeys for VRC FBX format. Retry searching viseme target for VRM format");
-                shapeKeyIndexes = GetVisemeTargetShapeKeyIndexes(morphTarget.skinnedMeshRenderer.sharedMesh, true);
-            }
 
-            if (shapeKeyIndexes.Count(v => v == -1) > 0)
+            // Map blend shapes
+            Dictionary<string, int> blendShapeMap = null;
+            if (IsVRCModel(morphTarget.skinnedMeshRenderer.sharedMesh))
             {
-                Debug.LogWarning("Can't get all shapekeys for VRM. Please configure viseme to blendshapes manually.");
-                shapeKeyIndexes = GetVisemeTargetShapeKeyIndexes(morphTarget.skinnedMeshRenderer.sharedMesh, true);
+                blendShapeMap = GetVRCBlendShapeMap(morphTarget.skinnedMeshRenderer.sharedMesh);
+            }
+            else if (IsVRMModel(morphTarget.skinnedMeshRenderer.sharedMesh))
+            {
+                blendShapeMap = GetVRMBlendShapeMap(morphTarget.skinnedMeshRenderer.sharedMesh);
             }
             else
             {
-                for (var i = 0; i < morphTarget.visemeToBlendTargets.Length; i++)
+                Debug.LogWarning("BlendShapes for VRC or VRM are not found. Set visemes manually to use LipSync.");
+            }
+
+            // Apply blend shapes
+            var visemes = Enum.GetNames(typeof(OVRLipSync.Viseme));
+            for (var i = 0; i < visemes.Length; i++)
+            {
+                if (blendShapeMap.ContainsKey(visemes[i]))
                 {
-                    if (shapeKeyIndexes[i] >= 0)
-                    {
-                        morphTarget.visemeToBlendTargets[i] = shapeKeyIndexes[i];
-                    }
+                    morphTarget.visemeToBlendTargets[i] = blendShapeMap[visemes[i]];
+                }
+                else
+                {
+                    morphTarget.visemeToBlendTargets[i] = 0;
                 }
             }
 
@@ -77,44 +79,151 @@ namespace ChatdollKit.Extension.OVR
             LipSyncContext = lipSyncContext;
         }
 
-        private List<int> GetVisemeTargetShapeKeyIndexes(Mesh mesh, bool IsVRM = false)
+        private Dictionary<string, int> GetVRCBlendShapeMap(Mesh mesh)
         {
-            var visemeTargetShapeKeyIndexes = new List<int>();
+            var blendShapeMap = GetBlendShapeMapBase();
 
-            // Get shapekeys of this model
-            var shapeKeys = new Dictionary<string, int>();
             for (var i = 0; i < mesh.blendShapeCount; i++)
             {
-                shapeKeys[mesh.GetBlendShapeName(i)] = i;
+                if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_sil"))
+                {
+                    blendShapeMap["sil"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_pp"))
+                {
+                    blendShapeMap["PP"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_ff"))
+                {
+                    blendShapeMap["FF"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_th"))
+                {
+                    blendShapeMap["TH"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_dd"))
+                {
+                    blendShapeMap["DD"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_kk"))
+                {
+                    blendShapeMap["kk"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_ch"))
+                {
+                    blendShapeMap["CH"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_ss"))
+                {
+                    blendShapeMap["SS"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_nn"))
+                {
+                    blendShapeMap["nn"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_rr"))
+                {
+                    blendShapeMap["RR"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_aa"))
+                {
+                    blendShapeMap["aa"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_e"))
+                {
+                    blendShapeMap["E"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_ih"))
+                {
+                    blendShapeMap["ih"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_oh"))
+                {
+                    blendShapeMap["oh"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("vrc.v_ou"))
+                {
+                    blendShapeMap["ou"] = i;
+                }
             }
 
-            // Set index of shapekeys
-            var visemeNames = Enum.GetNames(typeof(OVRLipSync.Viseme)); // Viseme names of OVRLipSync
-            for (var i = 0; i < visemeNames.Length; i++)
-            {
-                var visemeKey = visemeNames[i];
-                if (IsVRM)
-                {
-                    if (visemeKey.ToLower() == "aa") visemeKey = "a";
-                    else if (visemeKey.ToLower() == "ih") visemeKey = "i";
-                    else if (visemeKey.ToLower() == "ou") visemeKey = "u";
-                    else if (visemeKey.ToLower() == "e") visemeKey = "e";
-                    else if (visemeKey.ToLower() == "oh") visemeKey = "o";
-                    else visemeKey = "n";
-                }
-
-                var shapeKey = shapeKeys.Keys.Where(sk => sk.ToLower().Trim().EndsWith($"_{visemeKey}".ToLower())).FirstOrDefault();
-                if (!string.IsNullOrEmpty(shapeKey))
-                {
-                    visemeTargetShapeKeyIndexes.Add(shapeKeys[shapeKey]);
-                }
-                else
-                {
-                    visemeTargetShapeKeyIndexes.Add(-1);
-                }
-            }
-
-            return visemeTargetShapeKeyIndexes;
+            return blendShapeMap;
         }
+
+        private Dictionary<string, int> GetVRMBlendShapeMap(Mesh mesh)
+        {
+            var blendShapeMap = new Dictionary<string, int>();
+
+            for (var i = 0; i < mesh.blendShapeCount; i++)
+            {
+                if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_A"))
+                {
+                    blendShapeMap["aa"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_I"))
+                {
+                    blendShapeMap["ih"] = i;
+                    blendShapeMap["DD"] = i;
+                    blendShapeMap["kk"] = i;
+                    blendShapeMap["CH"] = i;
+                    blendShapeMap["SS"] = i;
+                    blendShapeMap["RR"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_U"))
+                {
+                    blendShapeMap["ou"] = i;
+                    blendShapeMap["PP"] = i;
+                    blendShapeMap["FF"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_E"))
+                {
+                    blendShapeMap["E"] = i;
+                    blendShapeMap["TH"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_O"))
+                {
+                    blendShapeMap["oh"] = i;
+                }
+                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_Close"))
+                {
+                    blendShapeMap["sil"] = i;
+                    blendShapeMap["nn"] = i;
+                }
+            }
+
+            return blendShapeMap;
+        }
+
+        private Dictionary<string, int> GetBlendShapeMapBase()
+        {
+            return Enum.GetNames(typeof(OVRLipSync.Viseme)).ToDictionary(v => v, v => 0);
+        }
+
+        private bool IsVRCModel(Mesh mesh)
+        {
+            for (var i = 0; i < mesh.blendShapeCount; i++)
+            {
+                if (mesh.GetBlendShapeName(i).Contains("vrc.v_"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsVRMModel(Mesh mesh)
+        {
+            for (var i = 0; i < mesh.blendShapeCount; i++)
+            {
+                if (mesh.GetBlendShapeName(i).Contains("Fcl_MTH_"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+#endif
     }
 }
