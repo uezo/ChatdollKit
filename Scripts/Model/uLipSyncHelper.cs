5448e695dbd0b9722a9d4e9305bedaa3da804bb5
uezo <uezo@uezo.net>
2023-03-21 00:36:31 +0900

    Separate uLipSyncHelper (for VRC and VRM)
    Helper for VRM supports runtime configuration🚗

--- Scripts/Model/uLipSyncHelper.cs
+++ Scripts/Model/uLipSyncHelper.cs
@@ -13,29 +13,28 @@ namespace ChatdollKit.Model
         }
 
 #if UNITY_EDITOR
-        public void ConfigureViseme()
+        public virtual void ConfigureViseme()
         {
             // Get GameObjects
             var modelController = gameObject.GetComponent<ModelController>();
+            if (modelController == null || modelController.AudioSource == null)
+            {
+                Debug.LogError("Add and setup ModelController before. You can retry setting up LipSync by selecting `Reset` in the context menu of OVRLipSyncHelper.");
+                return;
+            }
+
+            // Get BlendShapeMap for viseme
+            var blendShapeMap = GetBlendShapeMap(modelController);
+
+            // Get/Add uLipSyncBlendShape
             var uLipSyncBlendShape = gameObject.GetComponent<uLipSyncBlendShape>();
+            if (uLipSyncBlendShape == null)
+            {
+                uLipSyncBlendShape = gameObject.AddComponent<uLipSyncBlendShape>();
+            }
 
             // Configure uLipSyncBlendShape
             uLipSyncBlendShape.skinnedMeshRenderer = modelController.SkinnedMeshRenderer;
-
-            // Map blend shapes
-            Dictionary<string, int> blendShapeMap = null;
-            if (IsVRCModel(uLipSyncBlendShape.skinnedMeshRenderer.sharedMesh))
-            {
-                blendShapeMap = GetVRCBlendShapeMap(uLipSyncBlendShape.skinnedMeshRenderer.sharedMesh);
-            }
-            else if (IsVRMModel(uLipSyncBlendShape.skinnedMeshRenderer.sharedMesh))
-            {
-                blendShapeMap = GetVRMBlendShapeMap(uLipSyncBlendShape.skinnedMeshRenderer.sharedMesh);
-            }
-            else
-            {
-                Debug.LogWarning("BlendShapes for VRC or VRM are not found. Set visemes manually to use LipSync.");
-            }
 
             // Apply blend shapes
             uLipSyncBlendShape.blendShapes.Clear();
@@ -43,11 +42,32 @@ namespace ChatdollKit.Model
             {
                 uLipSyncBlendShape.blendShapes.Add(new uLipSyncBlendShape.BlendShapeInfo() { phoneme = map.Key, index = map.Value, maxWeight = 1 });
             }
+
+            // Get/Add uLipSync
+            var uLipSyncMain = gameObject.GetComponent<uLipSync.uLipSync>();
+            if (uLipSyncMain == null)
+            {
+                uLipSyncMain = gameObject.AddComponent<uLipSync.uLipSync>();
+            }
+
+            // Add listener
+            UnityEditor.Events.UnityEventTools.AddPersistentListener(uLipSyncMain.onLipSyncUpdate, uLipSyncBlendShape.OnLipSyncUpdate);
+
+            // Set profile
+            var profiles = UnityEditor.AssetDatabase.FindAssets("-Profile-Female");
+            if (profiles.Length > 0)
+            {
+                uLipSyncMain.profile = UnityEditor.AssetDatabase.LoadAssetAtPath<Profile>(UnityEditor.AssetDatabase.GUIDToAssetPath(profiles.First()));
+            }
         }
 
-        private Dictionary<string, int> GetVRCBlendShapeMap(Mesh mesh)
+        protected virtual Dictionary<string, int> GetBlendShapeMap(ModelController modelController)
         {
-            var blendShapeMap = GetBlendShapeMapBase();
+            var mesh = modelController.SkinnedMeshRenderer.sharedMesh;
+            var blendShapeMap = new Dictionary<string, int>()
+            {
+                { "A", 0 }, { "I", 0 }, { "U", 0 }, { "E", 0 }, { "O", 0 }, { "N", -1 }, { "-", -1 }
+            };
 
             for (var i = 0; i < mesh.blendShapeCount; i++)
             {
@@ -74,71 +94,6 @@ namespace ChatdollKit.Model
             }
 
             return blendShapeMap;
-        }
-
-        private Dictionary<string, int> GetVRMBlendShapeMap(Mesh mesh)
-        {
-            var blendShapeMap = GetBlendShapeMapBase();
-
-            for (var i = 0; i < mesh.blendShapeCount; i++)
-            {
-                if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_A"))
-                {
-                    blendShapeMap["A"] = i;
-                }
-                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_I"))
-                {
-                    blendShapeMap["I"] = i;
-                }
-                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_U"))
-                {
-                    blendShapeMap["U"] = i;
-                }
-                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_E"))
-                {
-                    blendShapeMap["E"] = i;
-                }
-                else if (mesh.GetBlendShapeName(i).EndsWith("Fcl_MTH_O"))
-                {
-                    blendShapeMap["O"] = i;
-                }
-            }
-
-            return blendShapeMap;
-        }
-
-        private Dictionary<string, int> GetBlendShapeMapBase()
-        {
-            return new Dictionary<string, int>()
-            {
-                { "A", 0 }, { "I", 0 }, { "U", 0 }, { "E", 0 }, { "O", 0 }, { "N", -1 }, { "-", -1 }
-            };
-        }
-
-        private bool IsVRCModel(Mesh mesh)
-        {
-            for (var i = 0; i < mesh.blendShapeCount; i++)
-            {
-                if (mesh.GetBlendShapeName(i).Contains("vrc.v_"))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private bool IsVRMModel(Mesh mesh)
-        {
-            for (var i = 0; i < mesh.blendShapeCount; i++)
-            {
-                if (mesh.GetBlendShapeName(i).Contains("Fcl_MTH_"))
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
 #endif
     }
