cc9850cb9c635b756b9b2549803a715b1da864eb
uezo <uezo@uezo.net>
2023-03-24 13:46:15 +0900

    Update demo and example for new animation style

--- Examples/ModelControl/Skills/ModelControlSkill.cs
+++ Examples/ModelControl/Skills/ModelControlSkill.cs
@@ -19,10 +19,10 @@ namespace ChatdollKit.Examples.ModelControl
             response.AddVoiceTTS("この声、");
 
             response.AddVoiceTTS("モーション、", postGap: 2.5f, asNewFrame: true);
-            response.AddAnimation("AGIA_Other_walking_01");
+            response.AddAnimation("BaseParam", 9, 3.0f);
 
             response.AddVoiceTTS("表情は、", postGap: 4.0f, asNewFrame: true);
-            response.AddAnimation("Default");
+            response.AddAnimation("BaseParam", 6);
             response.AddFace("Neutral", duration: 1.2f);
             response.AddFace("Smile", duration: 0.5f);
             response.AddFace("Angry", duration: 0.5f);
