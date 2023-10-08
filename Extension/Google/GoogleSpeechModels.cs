48fe20c53a4f1667498ccf6313f23b0775701b83
uezo <uezo@uezo.net>
2023-10-08 22:24:20 +0900

    Support favoring specific phrases on Google Text-to-Speech
    Set phrases and its boost value (1 to 20) on inspector of GoogleWakewordListener and GoogleVoiceRequestProvidor.
    
    https://cloud.google.com/speech-to-text/docs/reference/rest/v1p1beta1/RecognitionConfig#SpeechContext

---
+++ Extension/Google/GoogleSpeechModels.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using ChatdollKit.IO;
+
+namespace ChatdollKit.Extension.Google
+{
+    public class SpeechRecognitionRequest
+    {
+        public SpeechRecognitionConfig config;
+        public SpeechRecognitionAudio audio;
+
+        public SpeechRecognitionRequest(AudioClip audioClip, string languageCode, bool useEnhancedModel, List<SpeechContext> speechContexts, float[] samplingData = null)
+        {
+            config = new SpeechRecognitionConfig(audioClip, languageCode, useEnhancedModel, speechContexts);
+            audio = new SpeechRecognitionAudio(AudioConverter.AudioClipToBase64(audioClip, samplingData));
+        }
+    }
+
+    public class SpeechRecognitionConfig
+    {
+        public int encoding;
+        public double sampleRateHertz;
+        public double audioChannelCount;
+        public bool enableSeparateRecognitionPerChannel;
+        public string languageCode;
+        public string model;
+        public bool useEnhanced;
+        public List<SpeechContext> speechContexts;
+
+        public SpeechRecognitionConfig(AudioClip audioClip, string languageCode, bool useEnhancedModel, List<SpeechContext> speechContexts)
+        {
+            encoding = 1;   // 1: 16-bit linear PCM
+            sampleRateHertz = audioClip.frequency;
+            audioChannelCount = audioClip.channels;
+            enableSeparateRecognitionPerChannel = false;
+            this.languageCode = languageCode;
+            model = useEnhancedModel ? null : "default";
+            useEnhanced = useEnhancedModel;
+            this.speechContexts = speechContexts;
+        }
+    }
+
+    public class SpeechRecognitionAudio
+    {
+        public string content;
+
+        public SpeechRecognitionAudio(string content)
+        {
+            this.content = content;
+        }
+    }
+
+    [Serializable]
+    public class SpeechContext
+    {
+        public List<string> phrases;
+        public int boost;
+    }
+
+    public class SpeechRecognitionResponse
+    {
+        public SpeechRecognitionResult[] results;
+    }
+
+    public class SpeechRecognitionResult
+    {
+        public SpeechRecognitionAlternative[] alternatives;
+    }
+
+    public class SpeechRecognitionAlternative
+    {
+        public string transcript;
+        public double confidence;
+    }
+}
