7b0d9ecf5ae4317c95efc54ff920e9e7e5526ce0
uezo <uezo@uezo.net>
2024-07-20 23:16:51 +0900

    Enable runtime loading of VRM models from URL and byte data
    - Add functionality to load VRM format 3D models from a specified URL.
    - Implement support for loading VRM models from byte data obtained by any method.

--- Extension/VRM/VRMLoader.cs
+++ Extension/VRM/VRMLoader.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using UniGLTF;
 using VRM;
 using VRMShaders;
+using Newtonsoft.Json;
 using ChatdollKit.Model;
+using ChatdollKit.Network;
 
 namespace ChatdollKit.Extension.VRM
 {
@@ -16,6 +19,8 @@ namespace ChatdollKit.Extension.VRM
         private ModelController modelController;
         [SerializeField]
         private RuntimeAnimatorController animatorController;
+        [SerializeField]
+        private int WaitBeforeShowCharacter = 1000;
 
         public bool IsCharacterReady { get; private set; } = false;
         private RuntimeGltfInstance vrmInstance;
@@ -26,32 +31,54 @@ namespace ChatdollKit.Extension.VRM
         {
             Debug.LogWarning("This is an example to load VRM and configure ChatdollKit at runtime.");
 
-            if (!string.IsNullOrEmpty(VRMFilePath))
+            if (VRMFilePath.StartsWith("http://") || VRMFilePath.StartsWith("https://"))
             {
+                Debug.Log($"Get VRM from url: {VRMFilePath}");
+                _ = LoadCharacterAsync(VRMFilePath, "GET");
+            }
+            else
+            {
+                Debug.Log($"Get VRM from file: {VRMFilePath}");
                 _ = LoadCharacterAsync(VRMFilePath);
             }
         }
 
         public async UniTask LoadCharacterAsync(string path)
         {
+            var vrmBytes = File.ReadAllBytes(path);
+            await LoadCharacterAsync(vrmBytes);
+        }
+
+        public async UniTask LoadCharacterAsync(string url, string method = "GET", Dictionary<string, string> parameters = null, Dictionary<string, string> headers = null, Dictionary<string, object> jsonData = null)
+        {
+            var client = new ChatdollHttp();
+            var response = await client.SendRequestAsync(
+                url, method,
+                parameters: parameters,
+                headers: headers,
+                content: JsonConvert.SerializeObject(jsonData)
+            );
+
+            await LoadCharacterAsync(response.Data);
+        }
+
+        public async UniTask LoadCharacterAsync(byte[] vrmBytes)
+        {
             IsCharacterReady = false;
 
-            // Destroy current character and stop blinking
-            if (vrmInstance != null)
-            {
-                Destroy(vrmInstance);
-            }
-            if (characterObject != null)
-            {
-                Destroy(characterObject);
-            }
-
-            // Load character
             try
             {
-                // Load VRM from file
-                var vrmBytes = File.ReadAllBytes(path);
-                Debug.Log($"{path}: {vrmBytes.Length} bytes");
+                // Destroy current character and stop blinking
+                if (vrmInstance != null)
+                {
+                    Destroy(vrmInstance);
+                }
+                if (characterObject != null)
+                {
+                    Destroy(characterObject);
+                }
+
+                Debug.Log($"VRM size: {vrmBytes.Length} bytes");
 
                 // Parse data
                 var gltfData = new GlbBinaryParser(vrmBytes, "UserVrm").Parse();
@@ -73,6 +100,9 @@ namespace ChatdollKit.Extension.VRM
                 // Initialize ChatdollKit
                 modelController.gameObject.SetActive(true);
 
+                // Wait before show to finish transition to initial animation
+                await UniTask.Delay(WaitBeforeShowCharacter);
+
                 // Show character
                 vrmInstance.ShowMeshes();
 
@@ -81,7 +111,7 @@ namespace ChatdollKit.Extension.VRM
             }
             catch (Exception ex)
             {
-                Debug.LogError(ex.Message + ex.StackTrace);
+                Debug.LogError($"Error at LoadCharacterAsync: {ex.Message}\n{ex.StackTrace}");
             }
         }
     }
