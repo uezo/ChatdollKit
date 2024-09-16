a8eb13222122722c10bc79f1a31544cb72de3a28
uezo <uezo@uezo.net>
2024-09-16 09:31:36 +0900

    Add DialogPriorityManager for handling prioritized dialog requests
    Introduced `DialogPriorityManager` class to manage dialog requests based on priority levels.
    The manager can interrupt ongoing conversations or queue new ones for later, depending on the priority set in `SetRequest`. High-priority requests can immediately start, while lower-priority ones are queued and processed when idle. Queued dialogs can also be interrupted based on priority.
    
    Addresses issue #341.

---
+++ Scripts/Dialog/DialogPriorityManager.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using UnityEngine;
+using ChatdollKit.IO;
+
+namespace ChatdollKit.Dialog
+{
+    public class DialogPriorityManager : MonoBehaviour
+    {
+        private DialogProcessor dialogProcessor;
+        private PriorityQueue<DialogQueueItem> dialogQueue = new PriorityQueue<DialogQueueItem>();
+
+        [SerializeField]
+        private int idlingFrameThreshold = 2;
+        private int idlingFrameCount = 0;
+
+        private void Start()
+        {
+            dialogProcessor = gameObject.GetComponent<DialogProcessor>();
+        }
+
+        private void Update()
+        {
+            if (dialogProcessor == null) return;
+
+            if (dialogProcessor.Status == DialogProcessor.DialogStatus.Idling)
+            {
+                idlingFrameCount += 1;
+
+                if (idlingFrameCount >= idlingFrameThreshold)
+                {
+                    idlingFrameCount = 0;
+    
+                    if (!dialogQueue.IsEmpty())
+                    {
+                        var dialogRequest = dialogQueue.Dequeue();
+                        _ = dialogProcessor.StartDialogAsync(dialogRequest.Text, dialogRequest.Payloads);
+                    }
+                }
+            }
+            else
+            {
+                idlingFrameCount = 0;
+            }
+        }
+
+        public void SetRequest(string text, Dictionary<string, object> payloads = null, int priority = 10)
+        {
+            if (priority == 0)
+            {
+                _ = dialogProcessor.StartDialogAsync(text);
+            }
+            else
+            {
+                dialogQueue.Enqueue(new DialogQueueItem() {
+                    Priority = priority, Text = text, Payloads = payloads
+                }, priority);
+            }            
+        }
+
+        public void ClearDialogRequestQueue(int priority = 0)
+        {
+            dialogQueue.Clear(priority);
+        }
+
+        private class DialogQueueItem
+        {
+            public int Priority { get; set; }
+            public string Text { get; set; }
+            public Dictionary<string, object> Payloads { get; set; }
+        }
+    }
+}
