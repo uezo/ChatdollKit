142815ca0094dcceb8b4894f942ae73c21ed90e1
uezo <uezo@uezo.net>
2024-11-29 21:09:52 +0900

    Add UI component to for SocketServer
    - Start/Stop
    - Port number

---
+++ Scripts/UI/SocketServerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using ChatdollKit.Network;
+
+namespace ChatdollKit.UI
+{
+    public class SocketServerController : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField serverPortInput;
+        [SerializeField]
+        private Text serverStatusText;
+
+        private SocketServer socketServer;
+
+        private void Start()
+        {
+            socketServer = gameObject.GetComponent<SocketServer>();
+        }
+
+        private void Update()
+        {
+            serverStatusText.text = socketServer.IsRunning ? $"Listening: {socketServer.Port}" : "Stopped";
+        }
+
+        public void OnServerToggleButton()
+        {
+            if (socketServer.IsRunning)
+            {
+                // Stop server
+                socketServer.StopServer();
+            }
+            else
+            {
+                // Start server
+                var portStr = serverPortInput.text;
+                if (string.IsNullOrEmpty(portStr))
+                {
+                    portStr = "8888";
+                }
+
+                var port = int.Parse(portStr);
+                if (port < 1024 || port > 65535)
+                {
+                    Debug.LogError("Port number should be in range 1024 - 65535");
+                    return;
+                }
+
+                socketServer.StartServer(port);
+            }
+        }
+    }
+}
